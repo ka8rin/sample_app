fe0afd48178456038c5d879f6940e6ef3dd03b8a
Katerina Charilaou <kc00156@surrey.ac.uk>
2012-01-03 11:31:21 +0000

    Done with sign in

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -74,6 +74,11 @@ it "should have a welcome message" do
         post :create, :user => @attr
         flash[:success].should =~ /welcome to the sample app/i
       end
+
+it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
 end    
 
 	
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -41,6 +41,31 @@ describe "success" do
 
 end  #describe signup
 
+describe "sign in/out" do
+
+    describe "failure" do
+      it "should not sign a user in" do
+        visit signin_path
+        fill_in :email,    :with => ""
+        fill_in :password, :with => ""
+        click_button
+        response.should have_selector("div.flash.error", :content => "Invalid")
+      end
+    end
+
+    describe "success" do
+      it "should sign a user in and out" do
+        user = Factory(:user)
+        visit signin_path
+        fill_in :email,    :with => user.email
+        fill_in :password, :with => user.password
+        click_button
+        controller.should be_signed_in
+        click_link "Sign out"
+        controller.should_not be_signed_in
+      end
+    end
+  end #describe sign in/out
 #describe "GET /users" do
 # it "works! (now write some real specs)" do
 # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
