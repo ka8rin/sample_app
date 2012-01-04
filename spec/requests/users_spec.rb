9d720b4667cec0b405a38ebc5cbe25cf268d60ed
Katerina Charilaou <kc00156@surrey.ac.uk>
2012-01-04 20:57:25 +0000

    Finish user edit, update, index, and destroy actions

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -66,6 +66,8 @@ describe "sign in/out" do
       end
     end
   end #describe sign in/out
+
+
 #describe "GET /users" do
 # it "works! (now write some real specs)" do
 # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
